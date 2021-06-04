e888984839a080a2ba28edb31fa9598b87f1ca28
Full IM 28 정상규 <>
2021-06-04 02:15:48 +0000

    added background and jumping

--- scenes/main.js
+++ scenes/main.js
@@ -5,7 +5,7 @@ pos(20,20),
 body()
 ])
 
-const MOVE_SPEED = 200
+const MOVE_SPEED = 120
 keyDown('right', ()=>{
   player.move(MOVE_SPEED,0)
 })
@@ -14,17 +14,37 @@ keyDown('left', ()=>{
   player.move(-MOVE_SPEED,0)
 })
 
+keyDown('space', ()=>{
+ if(player.grounded()){
+   player.jump(player.jumpForce*0.7)
+ }
+})
+
+layers([
+		"bg",
+		"obj",
+		"ui",
+	], "obj");
+
+
+add([
+		sprite("sky"),
+		scale(width()/20, height()/50),
+		layer("bg"),
+	]);
+
 
 addLevel([
   '   @ ',
+  '  #  ',
   '     ',
-  '     ',
-  'xxxxx',
+  'xxxxxxxxxxx',
 ], {
   width:30,
-  height:48,
+  height:47,
+  '#':[sprite('dog'),body(),scale(0.5)],
   'x':[sprite('ground'),solid()],
-  '@':[sprite('anemy'), scale(0.5),body(),'dangerous']
+  '@':[sprite('enemy'), scale(0.5),body(),'dangerous']
 })
 
 player.collides('dangerous',()=>{
---
+++ scenes/practice.js
@@ -0,0 +1,21 @@
+// scene("practice", () => {
+//     // all objs are bound to a scene
+//     add(/* ... */)
+//     // all events are bound to a scene
+//     keyPress(/* ... */)
+// });
+
+
+
+
+// let fibonacci = function (n) {
+//   const memo = [0, 1];
+//   const aux = (n) => {
+//     // 이미 해결한 적이 있으면, 메모해둔 정답을 리턴한다.
+//     if (memo[n] !== undefined) return memo[n];
+//     // 새롭게 풀어야하는 경우, 문제를 풀고 메모해둔다.
+//     memo[n] = aux(n - 1) + aux(n - 2);
+//     return memo[n];
+//   };
+//   return aux(n);
+// };
